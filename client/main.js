010c2bca879b4469804371d63f498f8554d7dab1
Mario Flach <info@almightycouch.org>
2015-01-19 15:33:01 +0100

    Make sure that visitors have a user id

--- client/main.js
+++ client/main.js
@@ -2,8 +2,10 @@ Meteor.startup(function () {
     Meteor.subscribe('music');
     Tracker.autorun(function() {
         var userId = Meteor.userId();
-        if(!userId && P2P.peer) {
-        } else if(!P2P.peer) {
+        if(!userId) {
+            Meteor.loginVisitor();
+        }
+        if(!P2P.peer) {
             P2P.peer = new WebRTC(userId, { key: '62is9f6ozx2mx6r' });
             MusicManager.synchronize(function(error) {
                 console.warn(error);
