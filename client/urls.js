51a597b6926c111683f82ece28105507dfe19c62
Mario Flach <info@almightycouch.org>
2015-01-26 14:35:46 +0100

    Add support for live-searching collections
    We use the MongoDB $or operator and the built-in regular expression
    support (case-insensitive) to implement a simple text-search.

--- client/urls.js
+++ client/urls.js
@@ -13,18 +13,9 @@ Router.route('/music/', function() {
     var self = this;
     self.render('Collection', {
         data: {
-            model: function() {
-                var query = {};
-                var sort = { artist: 1, year: -1, album: 1, track: 1 };
-                var fields = { track: true, title: true, album: true, artist: true, duration: true, url: true, owner: true };
-                var collection = null;
-                if(self.params.hash == 'me') {
-                    collection = MusicManager.localCollection;
-                } else {
-                    collection = MusicCollection;
-                }
-                return collection.find(query, { fields: fields, sort: sort });
-            }
+            model: self.params.hash == 'me' ? MusicManager.localCollection : MusicCollection,
+            sort: { artist: 1, year: -1, album: 1, track: 1 },
+            fields: { track: true, title: true, album: true, artist: true, duration: true, url: true, owner: true }
         }
     });
 }, {
--- client/views/collection.js
+++ client/views/collection.js
@@ -1,8 +1,26 @@
-Template.Collection.rendered = function() {
+Template.Collection.created = function() {
     var self = this;
+    self.search = new ReactiveVar();
 }
 
+Template.Collection.helpers({
+    'songs': function() {
+        var self = Template.instance();
+        var search = self.search.get();
+        var query = {};
+        if(typeof search == 'string') {
+            var regexp = new RegExp(search.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&'), 'i');
+            _.extend(query, { '$or': [{ 'artist': regexp }, { 'album': regexp }, { 'title': regexp }]});
+        }
+        return this.model.find(query, { fields: this.fields, sort: this.sort });
+    }
+});
+
 Template.Collection.events({
+    'input input[role="search"]': function(event, template) {
+        var self = template;
+        self.search.set(event.target.value);
+    },
     'click table tbody tr': function(event, template) {
         var song = this;
         AudioPlayer.load(song, function() {
